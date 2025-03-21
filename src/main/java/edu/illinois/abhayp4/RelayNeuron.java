e5321ea17d2a448b2301b2f6a953e0bfe105420d
Abhay Pokhriyal <abhayp4@illinois.edu>
2025-03-21 12:57:25 -0500

    Changed variable names

--- src/main/java/edu/illinois/abhayp4/RelayNeuron.java
+++ src/main/java/edu/illinois/abhayp4/RelayNeuron.java
@@ -1,14 +1,14 @@
 package edu.illinois.abhayp4;
 
 public abstract class RelayNeuron implements Runnable {
-    public final Channel.SourceEndpoint source1, source2;
-    public final Channel.TargetEndpoint target1, target2;
+    public final TextChannel.SourceEndpoint source1, source2;
+    public final TextChannel.TargetEndpoint target1, target2;
     
     public RelayNeuron(
-        Channel.SourceEndpoint src1,
-        Channel.SourceEndpoint src2,
-        Channel.TargetEndpoint tgt1,
-        Channel.TargetEndpoint tgt2
+        TextChannel.SourceEndpoint src1,
+        TextChannel.SourceEndpoint src2,
+        TextChannel.TargetEndpoint tgt1,
+        TextChannel.TargetEndpoint tgt2
     ) {
         source1 = src1;
         source2 = src2;
--- src/main/java/edu/illinois/abhayp4/UnitNeuron.java
+++ src/main/java/edu/illinois/abhayp4/UnitNeuron.java
@@ -2,10 +2,10 @@ package edu.illinois.abhayp4;
 
 public class UnitNeuron extends RelayNeuron {
     public UnitNeuron(
-        Channel.SourceEndpoint src1,
-        Channel.SourceEndpoint src2,
-        Channel.TargetEndpoint tgt1,
-        Channel.TargetEndpoint tgt2
+        TextChannel.SourceEndpoint src1,
+        TextChannel.SourceEndpoint src2,
+        TextChannel.TargetEndpoint tgt1,
+        TextChannel.TargetEndpoint tgt2
     ) {
         super(src1, src2, tgt1, tgt2);
     }
