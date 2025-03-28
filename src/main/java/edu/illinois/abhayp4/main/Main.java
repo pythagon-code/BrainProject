8afde1b36aee38450bc2455cba4f03fc8b35cea1
Abhay Pokhriyal <abhayp4@illinois.edu>
2025-03-28 10:27:33 -0500

    Main now uses GlobalState for initialization

--- src/main/java/edu/illinois/abhayp4/main/Main.java
+++ src/main/java/edu/illinois/abhayp4/main/Main.java
@@ -13,7 +13,7 @@ import edu.illinois.abhayp4.*;
 
 public class Main {
     public static void main(String[] args) {
-        ModelClient.initialize(args[1], args[0]);
+        GlobalState.initialize(args);
 
         System.out.println("hello world");
         
