66b56aac1e7a3cb6011689239747dba5543091e0
Abhay Pokhriyal <abhayp4@illinois.edu>
2025-03-28 16:13:18 -0500

    Implement Closeable

--- src/main/java/edu/illinois/abhayp4/ModelClient.java
+++ src/main/java/edu/illinois/abhayp4/ModelClient.java
@@ -12,8 +12,8 @@ import java.util.List;
 
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.io.Closeable;
 import java.io.PrintWriter;
+import java.io.Closeable;
 import java.io.IOException;
 import java.io.IOError;
 
