ff6f3c45f94f35afcc66476c405bc0940905ab40
Abhay Pokhriyal <abhayp4@illinois.edu>
2025-04-06 17:45:41 -0500

    Start application

--- src/main/java/edu/illinois/abhayp4/main/Main.java
+++ src/main/java/edu/illinois/abhayp4/main/Main.java
@@ -9,9 +9,6 @@ import edu.illinois.abhayp4.Application;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(System.getProperty("user.dir"));
-        
-        Application app = new Application();
-        app.start();
+        Application.start();
     }
 }
