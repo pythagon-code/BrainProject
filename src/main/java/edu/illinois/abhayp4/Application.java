dd72f610ff07d051162a6cb154d59f390eba0884
Abhay Pokhriyal <abhayp4@illinois.edu>
2025-04-06 17:45:14 -0500

    Fix

--- src/main/java/edu/illinois/abhayp4/Application.java
+++ src/main/java/edu/illinois/abhayp4/Application.java
@@ -1,7 +1,70 @@
 package edu.illinois.abhayp4;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.IOError;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import org.yaml.snakeyaml.Yaml;
+
 public class Application {
-    public void start() {
+    private static Application app = null;
+
+    private final Map<String, Object> config;
+    final MainConfiguration mainConfig;
+    final ModelConfiguration modelConfig;
+    final OptimizationConfiguration optimizationConfig;
+
+    @SuppressWarnings("unchecked")
+    private Application() {
+        try (InputStream stream = new FileInputStream("application.yml")) {
+            Yaml yaml = new Yaml();
+            Map<String, Object> object = yaml.load(stream);
+            config = (Map<String, Object>) object.get("application");
+            mainConfig = null;
+            modelConfig = null;
+            optimizationConfig = null;
+
+            System.out.println("hello");
+            
+        }
+        catch (IOException e) {
+            throw new IOError(e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private <R> R getNestedField(String... path) {
+        Object current = config;
+        for (String field : path) {
+            if (current == null || !(current instanceof Map)) {
+                throw new NoSuchElementException("Field " + Arrays.toString(path) + " does not exist in application.yml");
+            }
+            current = ((Map<String, Object>) current).get(field); 
+        }
+        return (R) current;
+    }
+
+    public static void start() {
+        if (app != null) {
+            throw new IllegalStateException("Application already started");
+        }
         
+        app = new Application();
+    }
+
+    static class MainConfiguration {
+        
+    }
+
+    static class ModelConfiguration {
+
+    }
+
+    static class OptimizationConfiguration {
+
     }
 }
