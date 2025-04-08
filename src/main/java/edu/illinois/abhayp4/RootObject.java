8dccbd8751ea73bb76cdfe9ce5d71dee2cd4c61e
Abhay Pokhriyal <abhayp4@illinois.edu>
2025-04-07 22:52:46 -0500

    Add .equals()

--- src/main/java/edu/illinois/abhayp4/RootObject.java
+++ src/main/java/edu/illinois/abhayp4/RootObject.java
@@ -7,6 +7,8 @@ package edu.illinois.abhayp4;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 abstract class RootObject {
     @JsonProperty("Name") public final String name;
@@ -21,5 +23,19 @@ abstract class RootObject {
     @Override
     public String toString() {
         return getClass().getSimpleName() + " " + name;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            String thisJson = objectMapper.writeValueAsString(this);
+            String otherJson = objectMapper.writeValueAsString(other);
+            
+            return thisJson.equals(otherJson);
+        }
+        catch (JsonProcessingException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
