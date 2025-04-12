e7a745d23d32e0614f5910ce1b80f350edd43ba8
Abhay Pokhriyal <abhayp4@illinois.edu>
2025-04-11 21:27:04 -0500

    Update

--- src/main/java/edu/illinois/abhayp4/neurons/NeuronFactory.java
+++ src/main/java/edu/illinois/abhayp4/neurons/NeuronFactory.java
@@ -1,16 +1,32 @@
 package edu.illinois.abhayp4.neurons;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import edu.illinois.abhayp4.graphs.Graph;
+import edu.illinois.abhayp4.graphs.GraphNode;
 
 public class NeuronFactory {
-    private Map<RelayNeuron, List<RelayNeuron>> adjacency;
+    @JsonProperty("Graphs") private final Graph<RelayNeuron>[] graphs;
+    private final Map<RelayNeuron, List<RelayNeuron>> adjacency;
 
     public NeuronFactory(
-        Graph<?> graph
+        @JsonProperty("Graphs") Graph<RelayNeuron>[] graphs
     ) {
-        
+        this.graphs = graphs;
+        adjacency = new HashMap<>();
+        for (Graph<RelayNeuron> graph : graphs) {
+            for (GraphNode<RelayNeuron> node : graph) {
+                List<RelayNeuron> adjacentNodes = new ArrayList<>();
+                for (GraphNode<RelayNeuron> adjNode : node.getAdjacentNodes()) {
+                    adjacentNodes.add(adjNode.data);
+                }
+                adjacency.put(node.data, adjacentNodes);
+            }
+        }
     }
 }
